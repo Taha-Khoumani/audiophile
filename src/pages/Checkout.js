d7480c3485fcdd4599e9da209f1b7fd8281cbb98
tagopig <khoumanitaha23@gmail.com>
2023-02-06 21:41:26 -0500

    checkout validation

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -40,40 +40,67 @@ export default function Checkout() {
 
   const dispatch = useDispatch()
 
-  const padding = () => {
-    if (winWidth > 1195) {
-      return 165;
-    } else if (winWidth > 980) {
-      return 120;
-    } else if (winWidth > 870) {
-      return 90;
-    } else if (winWidth > 767) {
-      return 60;
-    } else if (winWidth < 767) {
-      return 40;
-    }
-};
+    const padding = () => {
+      if (winWidth > 1195) {
+        return 165;
+      } else if (winWidth > 980) {
+        return 120;
+      } else if (winWidth > 870) {
+        return 90;
+      } else if (winWidth > 767) {
+        return 60;
+      } else if (winWidth < 767) {
+        return 40;
+      }
+  };
 
-  function animateModal() {
-    if (isModalOpen) {
-      document.querySelector("#navbar").style.paddingRight = ``;
-      document.querySelector(
-        ".cart-container-1"
-      ).style.right = `${padding()}px`;
-      document.querySelector("#root").style.position = "static";
-      // document.querySelector(".cart").style.backgroundColor = "transparent";
-      // document.querySelector(".cart-content").classList.remove("cart-content-down");
-      // document.querySelector(".cart-content").classList.add("cart-content-up");
-      setTimeout(() => dispatch(toggleModal(false)), 500);
-    } else {
-      dispatch(toggleModal(true));
-    }
-}
+    function animateModal() {
+      if (isModalOpen) {
+        document.querySelector("#navbar").style.paddingRight = ``;
+        document.querySelector(
+          ".cart-container-1"
+        ).style.right = `${padding()}px`;
+        document.querySelector("#root").style.position = "static";
+        // document.querySelector(".cart").style.backgroundColor = "transparent";
+        // document.querySelector(".cart-content").classList.remove("cart-content-down");
+        // document.querySelector(".cart-content").classList.add("cart-content-up");
+        setTimeout(() => dispatch(toggleModal(false)), 500);
+      } else {
+        dispatch(toggleModal(true));
+      }
+  }
 
-function handleChange(e){
-  const {name,value} = e.target
-  dispatch(setData({name,value}))
-}
+  function handleChange(e){
+    const {name,value} = e.target
+    dispatch(setData({name,value}))
+  }
+
+  // function styleErrors(e){
+  //   if(!e.target.validity.valid)
+  //     document.querySelector(`[for="${e.target.id}"]`).style.color="#CD2C2C"
+  //   else
+  //     document.querySelector(`[for="${e.target.id}"]`).style.color = ``
+  // }
+
+  function styleErrors(){
+    let inputs = Array.from(document.querySelectorAll("[type='text'],[type='email'],[type='tel']"))
+    inputs.forEach(input=>{
+
+      //if the input is valid: set normal styling
+      if(input.validity.valid && input.classList.contains("invalid-input")){
+        input.classList.remove("invalid-input")
+        document.querySelector(`[for="${input.id}"]`).classList.remove("invalid-label")
+        document.querySelector(`[for="${input.id}"] span`).classList.remove("invalid-message")
+      }
+
+      //if the input is invalid: set red styling
+      else if(!input.validity.valid && !input.classList.contains("invalid-input")){
+        input.classList.add("invalid-input")
+        document.querySelector(`[for="${input.id}"]`).classList.add("invalid-label")
+        document.querySelector(`[for="${input.id}"] span`).classList.add("invalid-message")
+      }
+    })
+  }
 
   return (
     <>
@@ -81,25 +108,32 @@ function handleChange(e){
       <div id="checkout-page">
         <section id="checkout">
           <p className="checkout-header">CHECKOUT</p>
-          <form action="" className="form">
+          <form action="" className="form" id="checkout-form" onSubmit={(e)=>e.preventDefault()}>
             <div className="sub-form">
               <p className="sub-form-title">BILLING DETAILS</p>
               <div className="input-group-1">
                 <div className="input-container">
-                  <label htmlFor="name">Name</label>
+                  <label htmlFor="name">
+                    Name
+                    <span style={{display:"none"}}>required</span> 
+                  </label>
                   <input 
+                    required
                     type="text" 
                     id="name" 
                     placeholder="Taha.K"
                     name="name"
-                    onChange={(e)=>handleChange(e)}
+                    onChange={(e)=>{
+                      handleChange(e)
+                    }}
                     value={userData.name}
                   />
                 </div>
 
                 <div className="input-container">
-                  <label htmlFor="email-address">Email Address</label>
+                  <label htmlFor="email-address">Email Address<span style={{display:"none"}}>required</span></label>
                   <input
+                    required
                     type="email"
                     name="emailAddress"
                     id="email-address"
@@ -110,11 +144,13 @@ function handleChange(e){
                 </div>
 
                 <div className="input-container">
-                  <label htmlFor="phone-number">Phone Number</label>
+                  <label htmlFor="phone-number">Phone Number<span style={{display:"none"}}>required</span></label>
                   <input
-                    type="text"
+                    required
+                    type="tel"
                     id="phone-number"
                     placeholder="+1 111-111-111"
+                    pattern="[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}"
                     name="phoneNumber"
                     value={userData.phoneNumber}
                     onChange={(e)=>handleChange(e)}
@@ -126,8 +162,9 @@ function handleChange(e){
               <p className="sub-form-title">SHIPPING INFO</p>
               <div className="input-group-2">
                 <div className="input-container" id="address-container">
-                  <label htmlFor="address">Address</label>
+                  <label htmlFor="address">Address<span style={{display:"none"}}>required</span></label>
                   <input
+                    required
                     type="text"
                     id="address"
                     placeholder="2023 Silicon-Valey Avenu"
@@ -138,11 +175,13 @@ function handleChange(e){
                 </div>
 
                 <div className="input-container">
-                  <label htmlFor="zip-code">ZIP Code</label>
+                  <label htmlFor="zip-code">ZIP Code<span style={{display:"none"}}>required</span></label>
                   <input 
+                    required
                     type="text" 
                     id="zip-code" 
                     placeholder="X1Z K6N" 
+                    pattern="[a-z,A-z]\d[a-z,A-z]\s{0,1}\d[a-z,A-z]\d"
                     name="zipCode"
                     value={userData.zipCode}
                     onChange={(e)=>handleChange(e)}
@@ -150,8 +189,9 @@ function handleChange(e){
                 </div>
 
                 <div className="input-container">
-                  <label htmlFor="city">City</label>
+                  <label htmlFor="city">City<span style={{display:"none"}}>required</span></label>
                   <input 
+                    required
                     type="text" 
                     id="city" 
                     placeholder="Montreal" 
@@ -162,8 +202,9 @@ function handleChange(e){
                 </div>
 
                 <div className="input-container">
-                  <label htmlFor="country">Country</label>
+                  <label htmlFor="country">Country<span style={{display:"none"}}>required</span></label>
                   <input 
+                    required
                     type="text" 
                     id="country" 
                     placeholder="Canada" 
@@ -184,6 +225,7 @@ function handleChange(e){
                     style={userData.paymentMethod === "eMoney" ? {borderColor: "#d87d4a",borderWidth: "2px"} :{}}
                   >
                     <input
+                      required
                       type="radio"
                       name="paymentMethod"
                       value="eMoney"
@@ -198,6 +240,7 @@ function handleChange(e){
                     style={userData.paymentMethod === "cashOnDelivery" ? {borderColor: "#d87d4a",borderWidth: "2px"} :{}}
                   >
                     <input
+                      required
                       type="radio"
                       name="paymentMethod"
                       value="cashOnDelivery"
@@ -214,11 +257,13 @@ function handleChange(e){
                 userData.paymentMethod === "eMoney" &&
                 <div className="input-group-4">
                   <div className="input-container">
-                    <label htmlFor="e-money-number">e-Money Number</label>
+                    <label htmlFor="e-money-number">e-Money Number<span style={{display:"none"}}>required</span></label>
                     <input 
+                      required
                       type="text" 
                       id="e-money-number" 
                       placeholder="01010101" 
+                      pattern="\d{4,10}"
                       name="eMoneyNumber"
                       value={userData.eMoneyNumber}
                       onChange={(e)=>handleChange(e)}  
@@ -226,11 +271,13 @@ function handleChange(e){
                   </div>
 
                   <div className="input-container">
-                    <label htmlFor="e-money-pin">e-Money PIN</label>
+                    <label htmlFor="e-money-pin">e-Money PIN<span style={{display:"none"}}>required</span></label>
                     <input 
+                      required
                       type="text" 
                       id="e-money-pin" 
                       placeholder="0000" 
+                      pattern="\d{4}"
                       name="eMoneyPin"
                       value={userData.eMoneyPin}
                       onChange={(e)=>handleChange(e)}
@@ -262,14 +309,18 @@ function handleChange(e){
               <span> $ {(total + 50).toLocaleString()}</span>
             </p>
           </div>
-          <button 
+          <input
             className="button"
+            type="submit"
             onClick={()=>{
-              animateModal()
+              if(document.querySelector("#checkout-form").checkValidity())
+                animateModal()
+              else
+                styleErrors()
             }}
-          >
-            CONTINUE & PAY
-          </button>
+            value="CONTINUE & PAY"
+            form="checkout-form"
+          />
         </section>
         {
           isModalOpen 
